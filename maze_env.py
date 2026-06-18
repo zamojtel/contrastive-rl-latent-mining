58b1351ea5224ae02f02383c59112905f213c5bc
zamojtel <zamojtelbartek@gmail.com>
2026-06-18 17:03:46 +0200

    add multi-task training on 4 different maze layouts

--- maze_env.py
+++ maze_env.py
@@ -5,18 +5,17 @@ from brax.envs import State
 class MazeEnv:
     def __init__(self, maze):
         self.maze = maze
-        self.n_rows = len(maze)
-        self.n_cols = len(maze[0])
+        self.num_mazes, self.n_rows, self.n_cols = maze.shape 
         self.action_size = 4 
         self.state_dim = 2
         self.goal_indices = [0,1]
-        self.observation_size = 4 # bo 2 na current_pos i 2 na desired_goal
+        self.observation_size = 4
 
-        start = jnp.where(self.maze == 3)
-        finish = jnp.where(self.maze == 4)
-        
-        self.start_pos = (start[0][0], start[1][0]) 
-        self.goal = (finish[0][0], finish[1][0])
+        starts = jnp.where(self.maze == 3)
+        finishes = jnp.where(self.maze == 4)
+
+        self.start_positions = jnp.stack([starts[1], starts[2]], axis=-1)
+        self.goal_positions = jnp.stack([finishes[1], finishes[2]], axis=-1)
         
         self.action_map = jnp.array([
             [-1,0], # up
@@ -26,11 +25,13 @@ class MazeEnv:
         ])
 
     def reset(self, rng):
+        maze_id = jax.random.randint(rng, shape=(), minval=0, maxval=4)
+        current_start = self.start_positions[maze_id]
+        current_goal = self.goal_positions[maze_id]
+        obs = jnp.concatenate([current_start,current_goal])
         return State(
             pipeline_state=None,
-            obs = jnp.array(
-                self.start_pos+self.goal
-            ),
+            obs = obs,
             reward=jnp.zeros(()), 
             done=jnp.zeros(()),
             metrics={
@@ -39,11 +40,11 @@ class MazeEnv:
                 'dist': jnp.zeros(()),
                 'distance_from_origin': jnp.zeros(()),
             },
-            info={}
+            info={'maze_index': maze_id}
         )
         
     def step(self,state,action):
-        # przeliczanie wektorowe
+        current_maze_id = state.info['maze_index']
         action_idx = jnp.argmax(action)
         current_move = self.action_map[action_idx]
         current_row = state.obs[0]
@@ -54,25 +55,27 @@ class MazeEnv:
         is_valid_move =(
             ( new_row >= 0 ) & ( new_row < self.n_rows ) &
             ( new_col >= 0 ) & ( new_col < self.n_cols ) &
-            (self.maze[new_row,new_col] != 1 )
+            (self.maze[current_maze_id,new_row,new_col] != 1 )
         )
 
         final_row = jnp.where(is_valid_move,new_row,current_row)
         final_col = jnp.where(is_valid_move,new_col,current_col)
         next_state = (final_row,final_col)
-        
-        dist = jnp.abs(final_row-self.goal[0]) + jnp.abs(final_col-self.goal[1])
-        distance_from_origin = jnp.abs(final_row-self.start_pos[0]) + jnp.abs(final_col-self.start_pos[1])
 
-        is_done = ((final_row == self.goal[0]) & (final_col == self.goal[1]))
+        current_start = self.start_positions[current_maze_id]
+        current_goal = self.goal_positions[current_maze_id]
+        dist = jnp.abs(final_row-current_goal[0]) + jnp.abs(final_col-current_goal[1])
+        distance_from_origin = jnp.abs(final_row-current_start[0]) + jnp.abs(final_col-current_start[1])
+
+        is_done = ((final_row == current_goal[0]) & (final_col == current_goal[1]))
 
         reward = jnp.where(is_done,0.0,-1.0)
-        
+        current_pos = jnp.array([final_row,final_col])
+        obs = jnp.concatenate([current_pos,current_goal])
+
         return State(
             pipeline_state=None,
-            obs = jnp.array(
-                next_state+self.goal
-            ),
+            obs = obs,
             reward = reward,
             done = is_done.astype(jnp.float32),
             metrics = {**state.metrics,'success': is_done.astype(jnp.float32),
--- train.py
+++ train.py
@@ -20,46 +20,51 @@ def print_progress(num_steps, metrics,*args, **kwargs):
     print(f"Steps: {num_steps} | Success: {metrics['eval/episode_success']:.2f} | Dist to Goal: {metrics['eval/episode_dist']:.2f}")
 
 class Config:
-    # def __init__(self):
-    #     self.num_envs = 1
-    #     self.num_eval_envs = 1
-    #     self.episode_length = 257
-    #     self.action_repeat = 1
-    #     self.total_env_steps = 99_634
-    #     self.num_evals = 10
-    #     self.seed = 42
-    #     self.visualization_interval = 10
-    #     self.checkpoint_logdir = "./my_agent"
     def __init__(self):
         self.num_envs = 1024
         self.num_eval_envs = 256
         self.episode_length = 257
         self.action_repeat = 1
-        self.total_env_steps = 4888576
+        self.total_env_steps = 50_000_000
         self.num_evals = 10
         self.seed = 42
         self.visualization_interval = 10
         self.checkpoint_logdir = "./my_agent"
 
-# maze = jnp.array(
-#         [
-#             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#             [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#             [3, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 0],
-#             [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-#             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-#         ]
-#     )
-
-maze = jnp.array(
-        [
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-            [3, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        ]
-    )
+maze = jnp.array([
+# Labirynt 0
+[
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+],
+# Labirynt 1
+[
+[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+[3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4],
+[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+],
+# Labirynt 2
+[
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[3, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 4],
+[0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+],
+# Labirynt 3
+[
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+[3, 0, 1, 4, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
+])
 
 env = MazeEnv(maze)
 agent = CRL()
